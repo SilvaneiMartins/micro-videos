83ee7cf63ac61a9ad8f5e9c459918f2073c38e06
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2024-07-04 23:30:45 -0300

    feature: created fist entities category

---
+++ src/category/domain/entities.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+
+class Category:
+
+    def __init__(self, name: str, descrition: str, is_active: bool, created_at: datetime) -> None:
+        self.name = name
+        self.descrition = descrition
+        self.is_active = is_active
+        self.created_at = created_at
+
+
+print(vars(Category("Filmes", "Nome do filme", True, datetime.now())))
